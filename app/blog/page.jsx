7c78c8039783587d8c6f128c17d1f2182d8cca4b
Alfonso Graziano <alfonso.graziano@nearform.com>
2023-10-15 13:11:15 +0200

    WIP

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,3 +1,16 @@
-export default function BlogPage() {
-    return <div>Blog homepage</div>
-  }
+import { NotionAPI } from "notion-client";
+import NotionPage from "@/components/NotionPage";
+
+const blogHome = "905955f3-a757-483d-a4b4-d938ce5ee7df";
+
+export default async function BlogHome() {
+  const notion = new NotionAPI();
+
+  const recordMap = await notion.getPage(blogHome);
+
+  return (
+    <main>
+      <NotionPage recordMap={recordMap} />
+    </main>
+  );
+}
--- components/NotionPage.jsx
+++ components/NotionPage.jsx
@@ -4,5 +4,12 @@ import { NotionRenderer } from "react-notion-x";
 import "react-notion-x/src/styles.css";
 
 export default function NotionPage({ recordMap }) {
-  return <NotionRenderer recordMap={recordMap} />;
+  return (
+    <NotionRenderer
+      recordMap={recordMap}
+      mapPageUrl={(pageId) => `/blog/${pageId}`}
+      showTableOfContents={false}
+      fullPage={true}
+    />
+  );
 }
