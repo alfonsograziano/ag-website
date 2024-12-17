7f85ba9e48bc31e071ff12e1f942b2e9e2530491
alfonsograziano <alfonsohack0019@gmail.com>
2024-12-17 18:32:29 +0100

    minor updates

--- app/page.jsx
+++ app/page.jsx
@@ -5,18 +5,31 @@ import Contact from "@/components/contact";
 import Talks from "@/components/talks";
 import Certifications from "@/components/certifications";
 import dynamic from "next/dynamic";
+import { Suspense } from "react";
 
 const OneDay = dynamic(() => import("@/components/oneDay"));
 
 export default function Home() {
   return (
     <main className={styles.pageContainer}>
-      <MainSection />
-      <Certifications />
-      <WorkingAt />
-      <OneDay />
-      <Talks />
-      <Contact />
+      <Suspense>
+        <MainSection />
+      </Suspense>
+      <Suspense>
+        <Certifications />
+      </Suspense>
+      <Suspense>
+        <WorkingAt />
+      </Suspense>
+      <Suspense>
+        <OneDay />
+      </Suspense>
+      <Suspense>
+        <Talks />
+      </Suspense>
+      <Suspense>
+        <Contact />
+      </Suspense>
     </main>
   );
 }
