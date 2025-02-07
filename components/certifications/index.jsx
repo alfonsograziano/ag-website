5837c54f63a107a891c94ea5efc2c9221e60a468
alfonsograziano <alfonsohack0019@gmail.com>
2025-02-07 19:36:17 +0100

    new certfication

--- components/certifications/index.jsx
+++ components/certifications/index.jsx
@@ -4,6 +4,11 @@ import Image from "next/image";
 
 const certifications = [
   {
+    src: "/ml_engineer.png",
+    label: "AWS Machine Learning Engineer Associate",
+    url: "#", //TODO: Add credly badge as soon as the accounts are merged
+  },
+  {
     src: "/solutions_architect.png",
     label: "AWS Solutions Architect Associate",
     url: "https://www.credly.com/earner/earned/badge/45ce4d38-2a99-4690-b191-1791a5d33bf8",
@@ -17,11 +22,6 @@ const certifications = [
     src: "/cloud_practitioner.png",
     label: "AWS Cloud Practitioner",
     url: "https://www.credly.com/badges/2948d77f-e0c3-4f69-8303-a08cd2e8fc02",
-  },
-  {
-    src: "/google.png",
-    label: "Project Management by Google",
-    url: "https://www.coursera.org/account/accomplishments/specialization/certificate/3VA7RVRNAJ3L",
   },
 ];
 
