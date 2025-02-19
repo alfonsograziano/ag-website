e39a26d89dcfb1c4f7ac2a997c4925c151701c9e
alfonsograziano <alfonsohack0019@gmail.com>
2025-02-19 15:08:36 +0100

    talks refactoring + new talk

--- components/talks/index.jsx
+++ components/talks/index.jsx
@@ -17,101 +17,134 @@ function formatDate(dateString) {
 
 const buildDate = (day, month, year) => new Date(year, month - 1, day);
 
-const talks = [
+const talks = {
+  AUTOMATE_YOUR_LIFE: {
+    title: "Automate Your Life: Building a Workflow Engine in TypeScript",
+  },
+  STAFF_ENG_VS_ENG_MANAGER: {
+    title:
+      "The Staff Engineer VS Manager Path: A window over the tech career paths",
+  },
+  EFFORTLESS_SECURITY: {
+    title:
+      "Effortless Security: Simplify Authentication and Authorization with Open Source Tools",
+  },
+  PERCORSO_UNI: {
+    title:
+      "Dal Percorso Universitario al Successo Professionale: Consigli Pratici",
+  },
+  CREATING_VIDEOS: {
+    title: "Creating videos... With React!",
+  },
+  AI_GENERATED_UI: {
+    title: "AI-generated UI: The state of the Art",
+  },
+  FROM_SENIOR_ENG_TO_TL: {
+    title:
+      "From Senior Engineer to Tech Lead: close the gap and grow in the role	",
+  },
+};
+
+const talksInstances = [
+  {
+    eventName: "DWX - Developer Week '25",
+    talkTitle: talks.FROM_SENIOR_ENG_TO_TL.title,
+    date: buildDate(30, 6, 2025),
+    where: "Mannheim, Germany 🇩🇪",
+  },
+  {
+    eventName: "DWX - Developer Week '25",
+    talkTitle: talks.CREATING_VIDEOS.title,
+    date: buildDate(30, 6, 2025),
+    where: "Mannheim, Germany 🇩🇪",
+  },
   {
     eventName: "jsday 2025",
-    talkTitle: "Automate Your Life: Building a Workflow Engine in TypeScript",
+    talkTitle: talks.AUTOMATE_YOUR_LIFE.title,
     date: buildDate(7, 4, 2025),
     where: "Bologna, Italy 🇮🇹",
   },
   {
     eventName: "Devworld Conference",
-    talkTitle:
-      "The Staff Engineer VS Manager Path: A window over the tech career paths",
+    talkTitle: talks.STAFF_ENG_VS_ENG_MANAGER.title,
     date: buildDate(27, 2, 2025),
     where: "Amsterdam, Netherlands 🇳🇱",
   },
   {
     eventName: "Coderful",
-    talkTitle:
-      "Effortless Security: Simplify Authentication and Authorization with Open Source Tools",
+    talkTitle: talks.EFFORTLESS_SECURITY.title,
     date: buildDate(23, 1, 2025),
     where: "Catania, Italy 🇮🇹",
   },
   {
     eventName: "DevFest Pescara",
-    talkTitle:
-      "The Staff Engineer VS Manager Path: A window over the tech career paths",
+    talkTitle: talks.STAFF_ENG_VS_ENG_MANAGER.title,
     date: buildDate(9, 11, 2024),
     where: "Pescara, Italy 🇮🇹",
   },
   {
     eventName: "DevFest Bari",
-    talkTitle:
-      "Dal Percorso Universitario al Successo Professionale: Consigli Pratici",
+    talkTitle: talks.PERCORSO_UNI.title,
     date: buildDate(26, 10, 2024),
     where: "Bari, Italy 🇮🇹",
   },
   {
     eventName: "DevFest Modena",
-    talkTitle:
-      "The Staff Engineer VS Manager Path: A window over the tech career paths",
+    talkTitle: talks.STAFF_ENG_VS_ENG_MANAGER.title,
     date: buildDate(12, 10, 2024),
     where: "Modena, Italy 🇮🇹",
   },
   {
     eventName: "Come To Code",
-    talkTitle: "Creating videos... With React!",
+    talkTitle: talks.CREATING_VIDEOS.title,
     date: buildDate(29, 9, 2024),
     where: "Pignola, Italy 🇮🇹",
   },
   {
     eventName: "WeAreDevelopers World Congress",
-    talkTitle:
-      "The Staff Engineer VS Manager Path: A window over the tech career paths",
+    talkTitle: talks.STAFF_ENG_VS_ENG_MANAGER.title,
     date: buildDate(17, 7, 2024),
     where: "Berlin, Germany 🇩🇪",
   },
   {
     eventName: "React Summit",
-    talkTitle: "Creating videos... With React!",
+    talkTitle: talks.CREATING_VIDEOS.title,
     date: buildDate(18, 6, 2024),
     where: "Amsterdam, Netherlands 🇳🇱",
   },
   {
     eventName: "Road to Coderful",
-    talkTitle: "AI-generated UI: The state of the Art",
+    talkTitle: talks.AI_GENERATED_UI.title,
     date: buildDate(8, 6, 2024),
     where: "Naples, Italy 🇮🇹",
   },
   {
     eventName: "DevDays Europe",
-    talkTitle: "Creating videos... With React!",
+    talkTitle: talks.CREATING_VIDEOS.title,
     date: buildDate(23, 5, 2024),
     where: "Vilnius, Lithuania 🇱🇹",
   },
   {
     eventName: "RomaJS Meetup",
-    talkTitle: "Creating videos... With React!",
+    talkTitle: talks.CREATING_VIDEOS.title,
     date: buildDate(21, 2, 2024),
     where: "Rome, Italy 🇮🇹",
   },
   {
     eventName: "DevFest Bari",
-    talkTitle:
-      "The Staff Engineer VS Manager Path: A window over the tech career paths",
+    talkTitle: talks.STAFF_ENG_VS_ENG_MANAGER.title,
     date: buildDate(2, 12, 2023),
     where: "Bari, Italy 🇮🇹",
   },
   {
     eventName: "Conf42: JavaScript 2023",
-    talkTitle: "Creating videos... With React!",
+    talkTitle: talks.CREATING_VIDEOS.title,
     date: buildDate(16, 11, 2023),
     where: "Online",
   },
   {
     eventName: "DevFest Napoli",
-    talkTitle: "Creating videos... With React!",
+    talkTitle: talks.CREATING_VIDEOS.title,
     date: buildDate(28, 10, 2023),
     where: "Naples, Italy 🇮🇹",
   },
@@ -128,7 +161,7 @@ export default function Talks() {
     <section className={styles.center}>
       <h1 className={styles.title}>Talks</h1>
 
-      {talks.map((talk, index) => (
+      {talksInstances.map((talk, index) => (
         <div key={index} className={styles.containerShadow}>
           <div className={styles.container}>
             <span>
